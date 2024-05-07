61eb08473873c1b51ab5ea39ce81a121aee0a153
Marina Lomeu <156249375+marinalomeu@users.noreply.github.com>
2024-05-07 09:51:04 -0300

    Update heroesDB.js

--- front/heroesDB.js
+++ front/heroesDB.js
@@ -10,6 +10,11 @@ function heroesDB($http) {
         return promise;
     }
 
+    db.getStudios = function(){
+        const promise = $http.get('http://localhost:8083/heroes/studios/')
+        return promise;
+    }
+
     db.appendHero = function(hero){
         return $http.post('http://localhost:8083/heroes', hero)
     }
@@ -18,9 +23,15 @@ function heroesDB($http) {
         return $http.delete('http://localhost:8083/heroes/' + heroId)
     }
 
-
     db.editHero = function(hero){
         return $http.put('http://localhost:8083/heroes/' + hero.id, hero)
+    }
+
+    db.editHeroStudio = function(list, studio){
+        const obj = {ids : list, studio : studio}
+        console.log(obj)
+
+        return $http.put('http://localhost:8083/heroes/studio/',  obj)
     }
 
     db.updateSuperHeroesDataBase = function(heroesList){
@@ -28,7 +39,9 @@ function heroesDB($http) {
     }
 
     db.deleteHeroesGroup = function(list){
-        return $http.post('http://localhost:8083/heroes\/excluirlote/', list)
+        const obj = { ids : list };
+        
+        return $http.post('http://localhost:8083/heroes/excluirlote/', obj)
     }
 
 }
